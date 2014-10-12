4847d27319461a280a84dbea21a0e3204e0169b0
Alexander Witkowski <AlexanderWitkowski@gmail.com>
2014-10-12 21:50:28 +0200

    Changed ListBox to ListView to enable Binding to selected Item for connection Added DelegateCommand (needed for Button in MVVM) Added Button with Command Added fake Command handler to test Button and Binding to selected item

--- BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
+++ BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
@@ -1,21 +1,41 @@
 ﻿namespace BeeWiCar.Universal.ViewModels
 {
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Windows.Input;
+
+    using BeeWiCar.Universal.Common;
+    using BeeWiCar.Universal.Services;
 
     using Windows.Devices.Enumeration;
 
-    using BeeWiCar.Universal.Services;
-
     public class MainViewModel : BaseViewModel
     {
+        private ICommand connectCommand;
+
         private ObservableCollection<DeviceInformation> deviceInformations;
 
         private string hello;
 
+        private DeviceInformation selectedDevice;
+
         public MainViewModel()
         {
             this.Hello = "Hallo Leipzig";
+            this.ConnectCommand = new DelegateCommand<object>(this.HandleConnectCommand);
+        }
+
+        public ICommand ConnectCommand
+        {
+            get
+            {
+                return this.connectCommand;
+            }
+
+            set
+            {
+                this.SetProperty(ref this.connectCommand, value);
+            }
         }
 
         public ObservableCollection<DeviceInformation> DeviceInformations
@@ -44,11 +64,29 @@
             }
         }
 
+        public DeviceInformation SelectedDevice
+        {
+            get
+            {
+                return this.selectedDevice;
+            }
+
+            set
+            {
+                this.SetProperty(ref this.selectedDevice, value);
+            }
+        }
+
         public async void LoadData()
         {
             var bluetoothRf = new BluetoothRf();
             var bluetoothDeviceInformations = await bluetoothRf.EnumerateDevicesAsync();
             this.DeviceInformations = new ObservableCollection<DeviceInformation>(bluetoothDeviceInformations);
+        }
+
+        private void HandleConnectCommand(object obj)
+        {
+            Debug.WriteLine(this.SelectedDevice.Name);
         }
     }
 }
