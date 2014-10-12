a7de65915e211cbddbe797d1bb4f1ccf64470b5e
Alexander Witkowski <AlexanderWitkowski@gmail.com>
2014-10-12 20:18:01 +0200

    Hello Property in MainViewModel

--- BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
+++ BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
@@ -2,6 +2,24 @@
 {
     public class MainViewModel : BaseViewModel
     {
-       
+        private string hello;
+
+        public MainViewModel()
+        {
+            this.Hello = "Hallo Leipzig";
+        }
+
+        public string Hello
+        {
+            get
+            {
+                return this.hello;
+            }
+
+            set
+            {
+                this.SetProperty(ref this.hello, value);
+            }
+        }
     }
 }
