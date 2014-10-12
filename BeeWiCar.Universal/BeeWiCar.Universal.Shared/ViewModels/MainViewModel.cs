145b2ffbd8921691881332268e7b6063cba43f22
Alexander Witkowski <AlexanderWitkowski@gmail.com>
2014-10-12 20:57:41 +0200

    Added LoadData method to MainViewModel to fill ObservableCollection with device infos.

--- BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
+++ BeeWiCar.Universal/BeeWiCar.Universal.Shared/ViewModels/MainViewModel.cs
@@ -1,12 +1,34 @@
 ﻿namespace BeeWiCar.Universal.ViewModels
 {
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    using Windows.Devices.Enumeration;
+
+    using BeeWiCar.Universal.Services;
+
     public class MainViewModel : BaseViewModel
     {
+        private ObservableCollection<DeviceInformation> deviceInformations;
+
         private string hello;
 
         public MainViewModel()
         {
             this.Hello = "Hallo Leipzig";
+        }
+
+        public ObservableCollection<DeviceInformation> DeviceInformations
+        {
+            get
+            {
+                return this.deviceInformations;
+            }
+
+            set
+            {
+                this.SetProperty(ref this.deviceInformations, value);
+            }
         }
 
         public string Hello
@@ -20,6 +42,13 @@
             {
                 this.SetProperty(ref this.hello, value);
             }
+        }
+
+        private async void LoadData()
+        {
+            var bluetoothRf = new BluetoothRf();
+            var bluetoothDeviceInformations = await bluetoothRf.EnumerateDevicesAsync();
+            this.DeviceInformations = new ObservableCollection<DeviceInformation>(bluetoothDeviceInformations);
         }
     }
 }
