4f8a38e0b3d2212ed106d39dacf91d17bf2430b1
Alexander Witkowski <AlexanderWitkowski@gmail.com>
2014-10-12 20:42:45 +0200

    Added BluetoothRf-Service with help of DeviceInformation.FindAllAsync
    http://msdn.microsoft.com/en-us/library/br225435.aspx
    http://msdn.microsoft.com/en-us/library/windows/apps/xaml/Hh872189(v=win.10).aspx

---
+++ BeeWiCar.Universal/BeeWiCar.Universal.Shared/Services/BluetoothRf.cs
@@ -0,0 +1,18 @@
+﻿namespace BeeWiCar.Universal.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Windows.Devices.Bluetooth.Rfcomm;
+    using Windows.Devices.Enumeration;
+
+    public class BluetoothRf
+    {
+        public async Task<IEnumerable<DeviceInformation>> EnumerateDevicesAsync()
+        {
+            var deviceInformationCollection = await DeviceInformation.FindAllAsync(RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort));
+            return deviceInformationCollection;
+        }
+    }
+}
