44177bd8404fccb6fd2b367f9d099e6944d81a2c
Alexander Witkowski <AlexanderWitkowski@gmail.com>
2014-10-14 20:07:06 +0200

    Added car commands - let's steer this thing

---
+++ BeeWiCar.Universal/BeeWiCar.Universal.Shared/Data/BeeWiCarCommands.cs
@@ -0,0 +1,14 @@
+﻿namespace BeeWiCar.Universal.Data
+{
+    public static class BeeWiCarCommands
+    {
+        public const byte Forward_Stop = 0;
+        public const byte Forward_Go = 1;
+        public const byte Backwards_Stop = 2;
+        public const byte Backwards_Go = 3;
+        public const byte Left_Stop = 4;
+        public const byte Left_Go = 5;
+        public const byte Right_Stop = 6;
+        public const byte Right_Go = 7;
+    }
+}
